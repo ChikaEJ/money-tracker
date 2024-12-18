659d58c7e6a30af9c7a3e97a729668636e1de05a
Chingiz <ecd4gg@gmail.com>
2024-12-18 20:24:20 +0600

    Created Family instance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_18_142006) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_18_142343) do
+  create_table "families", force: :cascade do |t|
+    t.string "family_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
