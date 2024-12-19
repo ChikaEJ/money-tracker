6c1a4a5657aa8db2eb92ab846e096d8fe674cd37
Chingiz <ecd4gg@gmail.com>
2024-12-19 13:23:11 +0600

    add authenticat_user and change family helpers

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -23,12 +23,22 @@ class FamiliesController < ApplicationController
   end
 
   def new
-    if has_family? current_user
+    if !current_user.family_id.nil?
       redirect_to root_path
     else
       @family = Family.new
+      @families = Family.all
     end
+  end
 
+  def update_family_for_user
+    current_user.family_id = params[:family_id]
+    if current_user.save
+      redirect_to family_path(current_user.family_id)
+    else
+      flash.now[:error] = "Failed to update family."
+      render :new
+    end
   end
 
   def update
@@ -52,13 +62,10 @@ class FamiliesController < ApplicationController
   end
 
   def set_family
-    if current_user&.family_id
+    if current_user.family_id
       @family = Family.find(current_user.family_id)
-    else if current_user.family_id.nil?
+    else
       redirect_to new_family_path
-         else
-      redirect_to root_path
-         end
     end
   end
 end
